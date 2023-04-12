8142c6cdfa9ed1e06963b02aff31791c55136b31
Austin Sherron <austin@pryvate.email>
2023-04-11 17:04:28 -0700

    [Add] Add Support for Sessions Management
    * Add session mgr plugin
    * Add session mgr plugin keymappings
    * Add TODO to lualine plugin
    * Add stub AutoCmd lua util

---
+++ editors/nvim/lua/config/keymappings/sessionmgr.lua
@@ -0,0 +1,9 @@
+local km = require 'nvim.lua.utils.mapper'
+
+
+-- interactions ----------------------------------------------------------------
+
+km.nnoremap('<leader>ss', 'SessionManager save_current_session<cr>')
+km.nnoremap('<leader>sl', 'SessionManager load_last_session<cr>')
+km.nnoremap('<leader>sd', 'SessionManager load_current_dir_session<cr>')
+
