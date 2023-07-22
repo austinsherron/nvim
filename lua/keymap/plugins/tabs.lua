83d689821e966463511291063f3187e08c7a6e53
Austin Sherron <austin@pryvate.email>
2023-07-22 11:40:42 -0700

    [Feature] Add Key Bindings for Tabs

---
+++ lua/keymap/plugins/tabs.lua
@@ -0,0 +1,26 @@
+-- TODO: this file shouldn't be in keymap/plugins; figure out why it's not being read
+--       keymap
+
+local KM = require 'utils.core.mapper'
+
+
+-- TODO: refactor KeyMapper so that it can be instantiated w/ the state present in this
+--       function
+local function options(desc)
+    return { desc = 'tabs: ' .. desc, nowait = true }
+end
+
+-- navigation ------------------------------------------------------------------
+
+KM.nnoremap('<leader>tn', ':tabnext<CR>',     options('next'))
+KM.nnoremap('<leader>tp', ':tabprevious<CR>', options('previous'))
+
+-- core ops --------------------------------------------------------------------
+
+KM.nnoremap('<leader>to', ':tabnew<CR>',   options('open'))
+KM.nnoremap('<leader>tx', ':tabclose<CR>', options('close'))
+
+-- misc. ops -------------------------------------------------------------------
+
+KM.nnoremap('<leader>tl', ':tabs<CR>', options('list'))
+
--- lua/keymap/plugins/undotree.lua
+++ lua/keymap/plugins/undotree.lua
@@ -1,14 +1,13 @@
 local KM = require 'utils.core.mapper'
 
 
--- TODO: refactor KeyMapper so that it can be instantiated w/ the state present in this
---       function
-local function options(desc)
-  return { desc = 'undotree: ' .. desc, nowait = true }
-end
+local options = {
+    desc = 'undo: toggle',
+    nowait = true,
+    silent = true,
+}
 
 -- interactions ----------------------------------------------------------------
 
-KM.nnoremap('<leader>u', ':UndotreeToggle | UndotreeFocus<CR>', options('toggle'))
-KM.nnoremap('<leader>2', ':UndotreeShow | UndotreeFocus<CR>',   options('open'))
+KM.nnoremap('<leader>2', ':UndotreeToggle | UndotreeFocus<CR>', options)
 
