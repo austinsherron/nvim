31522c04b7f23a22bfd105b9233caac1e6a0f639
Austin Sherron <austin@pryvate.email>
2023-12-23 17:38:09 -0800

    [Fix] Update Indent Blankline Config to Conform to V3 API
    * Update indent-blankline plugin spec in service of aforementioned
      changes
    * Add interface util class w/ highlight builder
    * Add "surround" plugin to treesitter plugins enum
    * Update surround plugin spec w/ aforementioned enum entry

--- lua/plugins/config/interface/indent.lua
+++ lua/plugins/config/interface/indent.lua
@@ -1,26 +1,63 @@
+local Interface = require 'utils.api.vim.interface'
+
+local Highlight = Interface.Highlight
+
+local foreach = require('toolbox.utils.map').foreach
+
+local hooks = require 'ibl.hooks'
+
+
+local HIGHLIGHTS = {
+  Highlight.new('RainbowRed'):foreground('#E06C75' ),
+  Highlight.new('RainbowYellow'):foreground('#E5C07B' ),
+  Highlight.new('RainbowBlue'):foreground('#61AFEF' ),
+  Highlight.new('RainbowOrange'):foreground('#D19A66' ),
+  Highlight.new('RainbowGreen'):foreground('#98C379' ),
+  Highlight.new('RainbowViolet'):foreground('#C678DD' ),
+  Highlight.new('RainbowCyan'):foreground('#56B6C2' ),
+}
+
+local HIGHLIGHT_NAMES = Table.values(
+  HIGHLIGHTS, function(v, _) return v.name end
+)
+
+local function set_highlights()
+  foreach(HIGHLIGHTS, Interface.set_highlight)
+end
+
+
+local function register_highlights()
+  hooks.register(hooks.type.HIGHLIGHT_SETUP, set_highlights)
+end
 
 --- Contains functions for configuring the indent-blankline plugin.
 ---
 ---@class Indent
 local Indent = {}
 
----@return table: a table that contains configuration values for the indent-blankline plugin
+---@return table: a table that contains configuration values for the indent-blankline
+--- plugin
 function Indent.opts()
   return {
-    context_char = '┃',
-    context_highlight_list = {
-      'RainbowDelimiterViolet',
-      'RainbowDelimiterBlue',
-      'RainbowDelimiterCyan',
-      'RainbowDelimiterGreen',
-      'RainbowDelimiterYellow',
-      'RainbowDelimiterOrange',
-      'RainbowDelimiterRed',
+    indent = {
+      char = '┃',
     },
-    space_char_blankline = ' ',
-    show_current_context = true,
-    use_treesitter = true,
+    scope = {
+      highlight  = HIGHLIGHT_NAMES,
+      show_start = false,
+      show_end   = false,
+    },
   }
+end
+
+
+--- Configures the indent-blankline plugin.
+function Indent.config()
+  ---@note: set according to
+  ---       https://github.com/lukas-reineke/indent-blankline.nvim#multiple-indent-colors
+  register_highlights()
+
+  require('ibl').setup(Indent.opts())
 end
 
 return Indent
