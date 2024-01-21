5633c7d63979f531df240dec64ca60cc11501a4f
Austin Sherron <austin@pryvate.email>
2024-01-21 12:56:46 -0800

    [Feature] Add Key Binding To Close Help Windows

---
+++ lua/keymap/misc.lua
@@ -0,0 +1,15 @@
+
+local KeyMapper = require 'utils.core.mapper'
+local View = require 'utils.api.vim.view'
+
+local KM = KeyMapper.new({ nowait = true })
+
+-- filetype close --------------------------------------------------------------
+
+local function close_help()
+  View.close({ filetype = 'help' })
+end
+
+KM:bind_one('<leader>hq', close_help, { desc = 'close: help windows' })
+
+
