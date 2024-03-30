f07685e2e8f859effa89147bbff2eccffb36cd45
Austin Sherron <austin@pryvate.email>
2024-03-29 18:19:00 -0700

    [Fix] Update Spectre Command Construction Logic

--- lua/keymap/plugins/search/spectre.lua
+++ lua/keymap/plugins/search/spectre.lua
@@ -1,5 +1,8 @@
 local Editor = require 'utils.api.vim.editor'
 local KeyMapper = require 'utils.core.mapper'
+local Type = require 'toolbox.meta.type'
+
+local ViewMode = require('utils.api.vim.buffer').ViewMode
 
 local spectre = Lazy.require 'spectre' ---@module 'spectre'
 
@@ -16,9 +19,17 @@ local function spectre_cmd(cmd, select_word)
   end
 end
 
-local function make_open(cmd, view_mode, select_word)
-  cmd = cmd or Editor.window_aware_split('split').new
-  view_mode = view_mode or cmd
+local function open(cmd, select_word)
+  local view_mode
+
+  if String.is(cmd) or cmd == nil then
+    view_mode = Editor.window_aware_split()
+  elseif Type.is(cmd, ViewMode) then
+    view_mode = cmd.new
+    cmd = view_mode
+  else
+    Err.raise('unrecognized spectre cmd: %s', cmd)
+  end
 
   return function()
     --- NOTE: admittedly a hack to change opening position
@@ -30,24 +41,11 @@ end
 -- TODO: consider a more complete remapping of available actions:
 --       https://github.com/nvim-pack/nvim-spectre
 KM:bind({
-  { '<leader>so', make_open(), { desc = 'open' } },
-  { '<leader>sv', make_open('vnew', 'vsplit'), { desc = 'open in vsplit' } },
-  { '<leader>sh', make_open('new', 'split'), { desc = 'open in split' } },
-  {
-    '<leader>sw',
-    make_open('open_visual', nil, true),
-    { desc = 'search current word' },
-  },
-  {
-    '<leader>sw',
-    make_open('open_visual', nil, true),
-    { desc = 'search current word' },
-    { 'v' },
-  },
-  {
-    '<leader>sp',
-    make_open('open_file_search', nil, true),
-    { desc = 'search on current file' },
-  },
+  { '<leader>so', open(), { desc = 'open' } },
+  { '<leader>sv', open(ViewMode.VSPLIT), { desc = 'open in vsplit' } },
+  { '<leader>sh', open(ViewMode.SPLIT), { desc = 'open in split' } },
+  { '<leader>sw', open('open_visual', true), { desc = 'search word' } },
+  { '<leader>sw', open('open_visual', true), { desc = 'search current word' }, { 'v' } },
+  { '<leader>sp', open('open_file_search', true), { desc = 'search on current file' } },
   { '<leader>sx', spectre_cmd 'close', { desc = 'close' } },
 })
