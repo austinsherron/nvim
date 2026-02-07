4e5d2bd83834dac8d072f3b948eae5a1766fdbc6
Austin Sherron <austin@pryvate.email>
2026-02-07 14:40:59 -0500

    [Update] Remove Abbreviated Plugin Initialization

--- init.lua
+++ init.lua
@@ -2,10 +2,6 @@
 
 require 'utils.globals' -- import globals before doing anything else
 
----- init ----------------------------------------------------------------------
-
-require 'utils.plugins.initialize' -- for a fresh install
-
 ---- bootstrap -----------------------------------------------------------------
 
 Safe.require 'core.bootstrap' -- "bootstrap" settings must come first
