8a5c012975805dbeb998111ae73f10c822f2bf23
Austin Sherron <austin@pryvate.email>
2026-02-27 10:41:12 -0500

    [Update] Enable WakaTime

--- lua/plugins/spec/tools.lua
+++ lua/plugins/spec/tools.lua
@@ -89,8 +89,6 @@ return Plugins('tools', {
   ---- wakatime: track time spent programming
   {
     'wakatime/vim-wakatime',
-    -- NOTE: not using at the moment
-    enabled = false,
     lazy = false,
   },
 })
